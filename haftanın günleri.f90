27a7dd48aef193347639a405af5e7829549a91c2
zeinen <145760018+zeinen@users.noreply.github.com>
2024-01-01 22:00:24 +0300

    Add files via upload

---
+++ haftanın günleri.f90
@@ -0,0 +1,21 @@
+integer::x
+print*," bir tam sayý giriniz"
+read*,x
+if (x=1) then
+    print*,"pazartesi"
+else if(x=2)then
+    print*,"salý"
+else if(x=3)then
+    print*,"carsamba"
+else (x=4)then
+    print*,"persembe"
+else (x=5)then
+    print*,"cuma"
+else (x=6)then
+    print*,"cumartesi"
+else (x=7)then
+    print*,"pazar"
+else
+    print*,"girdiðiniz sayý gecerli degil"
+end if
+end program
